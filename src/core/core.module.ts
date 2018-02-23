b7e19c017935c9f716f10107f0bc90b0d65f62dc
Charles Jacquin <charles.jacquin@autistici.org>
2018-02-24 00:17:12 +0100

    feat(database): add and setup typeorm

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as path from 'path';
+import * as yenv from 'yenv';
 
 import { Env } from './env.component';
 
+const env = yenv();
+
 @Module({
-  exports: [Env],
+  exports: [
+    Env,
+    TypeOrmModule.forRoot({
+      entities: [path.join(process.cwd(), env.TYPEORM_ENTITIES)],
+      logging: env.TYPEORM_LOGGING,
+      synchronize: env.TYPEORM_SYNCHRONIZE,
+      type: env.TYPEORM_CONNECTION,
+      url: env.TYPEORM_URL,
+    }),
+  ],
 })
 export class CoreModule {}
