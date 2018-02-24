4263d25f7ecee59af6a90494173a1bad73d694ad
Charles Jacquin <charles.jacquin@autistici.org>
2018-02-24 23:57:19 +0100

    feat(core): Validation pipe

--- src/_core/index.ts
+++ src/_core/index.ts
@@ -1,19 +1,2 @@
-import { MiddlewaresConsumer, Module, NestModule } from '@nestjs/common';
-
-import { Context, ContextMiddleware } from './context';
-import { DatabaseModule } from './database';
-import { Env } from './env';
-import { Http } from './http';
-import { Logger, LoggerMiddleware } from './logger';
-
-@Module({
-  components: [Context, Env, Http, Logger],
-  exports: [Context, Env, Http, Logger],
-  imports: [DatabaseModule],
-})
-export class CoreModule implements NestModule {
-  public configure(consumer: MiddlewaresConsumer): void {
-    consumer.apply(ContextMiddleware).forRoutes({ path: '*' });
-    consumer.apply(LoggerMiddleware).forRoutes({ path: '*' });
-  }
-}
+export * from './core.module';
+export * from './validation';
