7ddc2daca961fad9b544e5f5865883bae7ce6fd6
Charles Jacquin <charles.jacquin@autistici.org>
2018-02-24 13:36:49 +0100

    fix(core): update log formater

--- src/_core/logger.component.ts
+++ src/_core/logger.component.ts
@@ -5,19 +5,17 @@ import { Env } from './env.component';
 
 const formatter = options =>
   options.meta && options.meta.requestId
-    ? `[RQID=${options.meta.requestId}] ${options.message}`
+    ? `[RQID=${options.meta.requestId}] ${process.pid} ${options.message}`
     : `${options.message}`;
 
 @Component()
 export class Logger extends WinstonLogger {
-  public static LOG_FILE = 'log/error.log';
-
   constructor(env: Env) {
     super({
       transports: [
         env.isProduction()
           ? new transports.File({
-              filename: Logger.LOG_FILE,
+              filename: env.LOG_ERROR_FILE,
               formatter,
               level: env.LOG_LEVEL,
             })
